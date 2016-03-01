b5e194c96a2aff35ca48323a8ecc576138b493ff
bogdan <bodikx@gmail.com>
2016-03-01 13:43:26 +0200

    - btn navigation with enter key handler; - progressbar; - answer handling;

--- app/js/app.js
+++ app/js/app.js
@@ -1,18 +1,25 @@
 (function () {
-    var questions = $('.form ul.questions');
+    var questions = $('.form ul.questions'),
+        progressBar = $('#progress'),
+        progressLabel = progressBar.find('.label span'),
+        progressPercent = progressBar.find('.bar .progress');
     // variables end;
 
     var init = function () {
         questionsInit();
+        progressBarInit();
         // add responsive margins
         responsiveMargins(10, 30);
         $(window).resize(responsiveMargins.bind(null, 10, 30));
         // add focus
         $(window).scroll(focusQuestion);
+        $(window).trigger('scroll');
         // btn navigation
-        $('.button.nav').on('click', navigationHandler);
+        $('.button-wrapper .button').on('click', navigationHandler);
         // enable btn navigation
         checkNavigationBtns(questions.find('>li.focus'));
+        // enter handler
+        $('body').on('keyup', enterHandler);
     },
 
     questionsInit = function () {
@@ -212,6 +219,14 @@
                         chCode++;
                     });
                     li.find('ul.columns').append(choicesList);
+                    li.find('ul.columns > li').on('click', function (event) {
+                        if (!questionModel.multiple) {
+                            questions.find('ul.columns > li').removeClass('selected');
+                            $(event.target).closest('li').addClass('selected');
+                        } else {
+                            $(event.target).closest('li').toggleClass('selected');
+                        }
+                    });
                     break;
 
                 case "images-list":
@@ -275,12 +290,34 @@
                         chCode++;
                     });
                     li.find('ul.columns').append(choicesList);
+                    li.find('ul.columns > li').on('click', function (event) {
+                        if (!questionModel.multiple) {
+                            questions.find('ul.columns > li').removeClass('selected');
+                            $(event.target).closest('li').addClass('selected');
+                        } else {
+                            $(event.target).closest('li').toggleClass('selected');
+                        }
+                    });
                     break;
             }
             questionsList.push(li);
         });
 
         questions.append(questionsList);
+    },
+
+    progressBarInit = function () {
+        var allCount = questions.find('>li:not([class*="statement"])').length,
+            answeredCount = questions.find('>li.answered').length,
+            percent = answeredCount*100/allCount;
+
+        progressLabel.text(answeredCount + ' of '+ allCount +' answered');
+        progressPercent.css('width', percent+'%');
+
+        // enable submit btn
+        if (percent === 100) {
+            $('#unfixed-footer .submit .button').removeClass('disabled').addClass('enabled');
+        }
     },
 
     responsiveMargins = function (marginTopPercent, marginBottomPercent) {
@@ -300,25 +337,34 @@
                 $(question).removeClass('focus');
             }
         });
+
+        // submit is visible?
+        var submitHeight = $(document).height() - ($(window).scrollTop() + window.innerHeight);
+        if (submitHeight <= parseInt(window.innerHeight/100*30)) {
+            $('#unfixed-footer').show(500);
+        } else if ($('#unfixed-footer').is(':visible')) {
+            $('#unfixed-footer').hide(500);
+        }
     },
 
     checkNavigationBtns = function (currentEl) {
         if ($(currentEl).prev().length) {
-            $('.button-wrapper.up .button.nav').removeClass('disabled').addClass('enabled')
+            $('.button-wrapper.up .button.nav').removeClass('disabled').addClass('enabled');
         } else {
-            $('.button-wrapper.up .button.nav').removeClass('enabled').addClass('disabled')
+            $('.button-wrapper.up .button.nav').removeClass('enabled').addClass('disabled');
         }
 
         if ($(currentEl).next().length) {
-            $('.button-wrapper.down .button.nav').removeClass('disabled').addClass('enabled')
+            $('.button-wrapper.down .button.nav').removeClass('disabled').addClass('enabled');
         } else {
-            $('.button-wrapper.down .button.nav').removeClass('enabled').addClass('disabled')
+            $('.button-wrapper.down .button.nav').removeClass('enabled').addClass('disabled');
         }
     },
 
     navigationHandler = function (event) {
         var btnType = $(event.target).closest('div.button-wrapper')[0],
             btnType = btnType.classList[btnType.classList.length-1];
+
 
         switch (btnType) {
             case "up":
@@ -330,12 +376,75 @@
                 break;
 
             default:
+                var currentQuestion = questions.find('>li.focus');
+                    questType = currentQuestion.data('model').type;
+
+                if (!currentQuestion.hasClass('answered')) {
+                    switch (questType) {
+                        case "statement":
+                            break;
+
+                        case "textfield":
+                            currentQuestion.find('.message ').html('');
+
+                            if (currentQuestion.find('.input input').val()) {
+                                // sending api request
+                                currentQuestion.addClass('answered');
+                                currentQuestion.find('.input input').prop('readonly', true);
+                                progressBarInit();
+
+                            } else {
+                                currentQuestion.find('.input input').focus();
+                                currentQuestion.find('.message ').html('<p style="color: red;margin: 5px 0;">Sorry, but you must to answer...</p>');
+                                return;
+                            }
+                            break;
+
+                        case "list":
+                        case "images-list":
+                            currentQuestion.find('.message ').html('');
+
+                            if (currentQuestion.find('ul.columns > li.selected').length) {
+                                // sending api request
+                                currentQuestion.addClass('answered');
+                                currentQuestion.find('ul.columns > li').off('click');
+                                progressBarInit();
+                            } else {
+                                currentQuestion.find('.message ').html('<p style="color: red;margin: 5px 0;">Sorry, but you must to answer...</p>');
+                                return;
+                            }
+                            break;
+                    }
+                }
+
                 if ($(event.target).closest('div.button.nav').hasClass('enabled')) {
                     questions.find('>li.focus').next().length &&  $('body').animate({scrollTop: questions.find('>li.focus').next().offset().top}, '500',function(){
                         //DO SOMETHING AFTER SCROLL ANIMATION COMPLETED
                     });
                 }
                 break;
+
+            case "submit":
+                var currentQuestion = questions.find('>li.focus');
+
+                if (!currentQuestion.hasClass('answered')) {
+                    currentQuestion.find('.button.nav').trigger('click');
+                }
+
+                if ($(event.target).closest('div.button').hasClass('enabled')) {
+                    // sending api request
+                }
+                break;
+        }
+    },
+
+    enterHandler = function (event) {
+        if (event.which === 13) {
+            if (questions.find('>li.focus').next().length) {
+                questions.find('>li.focus .button-wrapper .button').trigger('click');
+            } else {
+                $('.button-wrapper.submit .button').trigger('click');
+            }
         }
     };
     // functions end;
